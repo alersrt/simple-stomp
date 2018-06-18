c2e376b34402df409a8f386d635e1a45ef71044b
alersrt <strtmg@yandex.ru>
2018-06-19 01:20:52 +0700

    Add websocket configuration

---
+++ src/main/java/org/student/simplestomp/config/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package org.student.simplestomp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+
+  @Bean
+  public WebSocketMessageBrokerConfigurer webSocketMessageBrokerConfigurer() {
+    return new WebSocketMessageBrokerConfigurer() {
+
+      @Override
+      public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/message");
+        registry.setApplicationDestinationPrefixes("/app");
+      }
+
+      @Override
+      public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").withSockJS();
+      }
+    };
+  }
+}
